d2a68f615648214706ed15bead10804c42ac41d9
Mackenzie Child <mackenzie.child@gmail.com>
2014-11-22 14:58:46 -0700

    Set images to crop excess on upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 	has_many :comments
-	has_attached_file :image, styles: { medium: "700x550", small: "400x350" }
+	has_attached_file :image, styles: { medium: "700x500#", small: "350x250#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
