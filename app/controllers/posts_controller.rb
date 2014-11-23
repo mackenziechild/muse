f711ed29a20588673e34945bd72a0c18aa51cc59
Mackenzie Child <mackenzie.child@gmail.com>
2014-11-23 14:42:50 -0700

    Finish styling show page & fix random post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,8 @@ class PostsController < ApplicationController
 
 	def show
 		@comments = Comment.where(post_id: @post)
+		@random_post = Post.where.not(id: @post).order("RANDOM()").first
+
 	end
 
 	def new
